e1db5ac72c9ea37627850d76619907f5448a2835
Jonathan Silva <jonathan.silva167@gmail.com>
2024-07-23 23:49:02 -0400

    - Clear boilerplate - Add mixins, reset, variables, and globals - Add Sass and Axios

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.scss";
+
+export default function Home() {
+	return (
+		<main className={styles.main}>
+			<h1>Mentor Me</h1>
+		</main>
+	);
+}
